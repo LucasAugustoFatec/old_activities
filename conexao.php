012c77a5d9a9bb5f586d778d94a932eefcd194ca
LucasAugustoFatec <106602796+LucasAugustoFatec@users.noreply.github.com>
2022-06-07 22:31:47 -0300

    Create conexao.php

---
+++ conexao.php
@@ -0,0 +1,13 @@
+<?php
+define('HOST', 'localhost');
+define('USUARIO', 'id18911902_root');
+define('SENHA', '1TS2%0nS/dFiw[~p');
+define('DB', 'id18911902_augusto_fatec');
+/*
+$conn = mysqli_connect(HOST, USUARIO, SENHA, DB)
+or
+die ('Ops! Não foi posível realizar a conexão...');
+
+echo "A conexão foi bem sucedida. ";
+*/
+?>
